1068021c52a1c2eae0bc2459b919d18868e92fed
Reece Hebel <reecehebel@yahoo.com>
2017-02-16 16:26:07 -0600

    create some classes for the data.

---
+++ ItemScraper/Classes/Skills.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemScraper.Classes
+{
+    class Skills
+    {
+        public string Name { get; set; }
+        public bool Support { get; set; }
+        public bool Mine { get; set; }
+        public string MainAttribute { get; set; }
+    }
+}
--- ItemScraper/Classes/Url.cs
+++ ItemScraper/Classes/Url.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace ItemScraper.Classes
 {
     //ideally I would rather this be in a database but I don't have one yet so this is just the list of links I found to scrape
-    //urls there is no data for: flasks, uniques
+    //urls there is no data for: flasks, uniques, jewels, 
     class Url
     {
         public Dictionary<string,int> URLS = new Dictionary<string,int>(); //using the int to filter by type of html returned. I'm sure it's not the best way but better than checking each url by string
@@ -24,6 +24,7 @@ namespace ItemScraper.Classes
             URLS.Add("https://www.pathofexile.com/skills/intelligence", 8);
             URLS.Add("https://www.pathofexile.com/skills/dexterity", 9);
             URLS.Add("https://www.pathofexile.com/skills/strength", 10);
+            URLS.Add("https://www.pathofexile.com/skills/remoteskills", 11);
 
         }
     }
